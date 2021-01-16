6bef697856518d8c549b1b9e32d9752d876414ce
Matt Brown <matt.brown@kitware.com>
2021-01-16 12:52:05 -0500

    Remove references to unused image_topic and frame_id.

---
+++ docker/colmap/image_undistorter.sh
@@ -0,0 +1,14 @@
+# Pass the path to the Colmap data folder.
+
+docker pull colmap/colmap:latest
+
+docker run -it \
+    --gpus all \
+    --network="host" \
+    -e "DISPLAY" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -w /working \
+    -v $1:/working \
+    colmap/colmap:latest \
+    colmap image_undistorter \
+        --input_path sparse/$2 \
