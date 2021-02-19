1c07827127f35925e55e5e929a5d32a486e7c41e
Matt Brown <matt.brown@kitware.com>
2021-02-18 21:05:40 -0500

    Fix issue with shape of outputs from cv2.undistortPoints.

--- src/colmap_processing/camera_models.py
+++ src/colmap_processing/camera_models.py
@@ -1041,9 +1041,9 @@ class StandardCamera(Camera):
 
         # Unproject rays into the camera coordinate system.
         ray_dir = np.ones((3,points.shape[1]), dtype=points.dtype)
-        ray_dir0 = cv2.undistortPoints(np.expand_dims(points.T, 0),
+        ray_dir0 = cv2.undistortPoints(np.expand_dims(points.T, 1),
                                        self._K, self._dist, R=None)
-        ray_dir[:2] = np.squeeze(ray_dir0, 0).T
+        ray_dir[:2] = np.squeeze(ray_dir0, 1).T
 
         # Rotate rays into the navigation coordinate system.
         ray_dir = np.dot(quaternion_matrix(self._cam_quat)[:3,:3], ray_dir)
@@ -1509,9 +1509,9 @@ class GeoStaticCamera(DepthCamera):
 
         # Unproject rays into the camera coordinate system.
         ray_dir = np.ones((3,points.shape[1]), dtype=points.dtype)
-        ray_dir0 = cv2.undistortPoints(np.expand_dims(points.T, 0),
+        ray_dir0 = cv2.undistortPoints(np.expand_dims(points.T, 1),
                                        self.K, self.dist, R=None)
-        ray_dir[:2] = np.squeeze(ray_dir0, 0).T
+        ray_dir[:2] = np.squeeze(ray_dir0, 1).T
 
         # Rotate rays into the local east/north/up coordinate system.
         ray_dir = np.dot(self.R.T, ray_dir)
